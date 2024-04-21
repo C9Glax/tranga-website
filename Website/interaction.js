a3479bcf8b313e217abd23fb9b8213c379735710
db-2001 <adityabhatnagar2001@gmail.com>
2024-04-21 14:18:30 -0400

    C9Glax/tranga-website#79

--- Website/interaction.js
+++ Website/interaction.js
@@ -15,6 +15,7 @@ let statusNameMatch = [];
 const searchBox = document.querySelector("#searchbox");
 const settingsPopup = document.querySelector("#settingsPopup");
 const filterBox = document.querySelector("#filterBox");
+const filterContent = document.querySelector("#filterContent");
 const settingsCog = document.querySelector("#settingscog");
 const filterFunnel = document.querySelector("#filterFunnel");
 const tasksContent = document.querySelector("content");
@@ -183,6 +184,15 @@ function Setup(){
   statusMatch.length = 0;
 }
 Setup();
+
+function ToggleFilterBox() {
+  if (filterBox.style.display == 'none') {
+    filterBox.style.display = 'flex';
+  } else {
+    filterBox.style.display = 'none';
+  }
+  filterContent.scrollTop = 0;
+}
 
 function ToggleFilterConnector(connector, event) {
   //console.log("Initial Array:");
@@ -508,7 +518,7 @@ settingsCog.addEventListener("click", () => {
 });
 
 filterFunnel.addEventListener("click", () => {
-  filterBox.classList.toggle("animate");
+  ToggleFilterBox();
 });
 
 settingKomgaUrl.addEventListener("keypress", (event) => { if(event.key === "Enter") UpdateSettings(); });
