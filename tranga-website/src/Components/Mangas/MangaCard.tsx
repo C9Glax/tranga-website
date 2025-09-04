097e991aec616cea96a39a14281731af56152691
glax <johanna@bernloehr.eu>
2025-09-05 00:47:42 +0200

    prettier

--- tranga-website/src/Components/Mangas/MangaCard.tsx
+++ tranga-website/src/Components/Mangas/MangaCard.tsx
@@ -1,9 +1,21 @@
-import {Badge, Card, CardContent, CardCover, ColorPaletteProp, Skeleton, Typography,} from '@mui/joy'
-import {EventHandler, ReactNode, useContext} from 'react'
+import {
+    Badge,
+    Card,
+    CardContent,
+    CardCover,
+    ColorPaletteProp,
+    Skeleton,
+    Typography,
+} from '@mui/joy'
+import { EventHandler, ReactNode, useContext } from 'react'
 import './MangaCard.css'
 import MangaConnectorIcon from './MangaConnectorIcon.tsx'
-import {Manga, MangaReleaseStatus, MinimalManga} from '../../api/data-contracts.ts'
-import {ApiContext} from '../../contexts/ApiContext.tsx'
+import {
+    Manga,
+    MangaReleaseStatus,
+    MinimalManga,
+} from '../../api/data-contracts.ts'
+import { ApiContext } from '../../contexts/ApiContext.tsx'
 
 export default function MangaCard(props: MangaCardProps): ReactNode {
     const Api = useContext(ApiContext)
@@ -16,7 +28,9 @@ export default function MangaCard(props: MangaCardProps): ReactNode {
                 />
             ))}
             className={'manga-card-badge'}
-            color={releaseColor(props.manga?.releaseStatus??MangaReleaseStatus.Unreleased)}
+            color={releaseColor(
+                props.manga?.releaseStatus ?? MangaReleaseStatus.Unreleased
+            )}
         >
             <Card className={'manga-card'} onClick={props.onClick}>
                 <CardCover className={'manga-card-cover'}>
@@ -50,13 +64,19 @@ const stringWithRandomLength = (): string => {
     return 'wow'
 }
 
-const releaseColor = (status : MangaReleaseStatus) : ColorPaletteProp => {
+const releaseColor = (status: MangaReleaseStatus): ColorPaletteProp => {
     switch (status) {
-        case MangaReleaseStatus.Cancelled: return 'danger';
-        case MangaReleaseStatus.Completed: return 'success';
-        case MangaReleaseStatus.Unreleased: return 'neutral';
-        case MangaReleaseStatus.Continuing: return 'primary';
-        case MangaReleaseStatus.OnHiatus: return 'neutral';
-        default: return 'neutral';
+        case MangaReleaseStatus.Cancelled:
+            return 'danger'
+        case MangaReleaseStatus.Completed:
+            return 'success'
+        case MangaReleaseStatus.Unreleased:
+            return 'neutral'
+        case MangaReleaseStatus.Continuing:
+            return 'primary'
+        case MangaReleaseStatus.OnHiatus:
+            return 'neutral'
+        default:
+            return 'neutral'
     }
 }
