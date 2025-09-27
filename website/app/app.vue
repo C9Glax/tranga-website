d99dcb8c774f748bd4b9ba12086f1232e7fbb142
glax <johanna@bernloehr.eu>
2025-09-27 22:07:47 +0200

    Add links to github, home

--- website/app/app.vue
+++ website/app/app.vue
@@ -1,7 +1,13 @@
 <template>
     <UApp>
         <UHeader>
-            <template #title>
+            <template #left>
+                <NuxtLink to="https://github.com/C9Glax/tranga"><Icon name="i-lucide-github" />API</NuxtLink>
+                <NuxtLink to="https://github.com/C9Glax/tranga-website"><Icon name="i-lucide-github" />Website</NuxtLink>
+                <NuxtLink to=""><Icon name="i-lucide-book-open" />Swagger</NuxtLink>
+            </template>
+            <template #default>
+                <NuxtLink to="/">
                 <div class="h-full flex gap-2 items-center">
                     <img src="/blahaj.png" class="h-lh cursor-grab" />
                     <p
@@ -10,10 +16,11 @@
                             -webkit-background-clip: text;
                             -webkit-text-fill-color: transparent;
                         "
-                        class="font-bold cursor-default text-3xl">
+                        class="font-bold cursor-pointer text-3xl">
                         Tranga
                     </p>
                 </div>
+                </NuxtLink>
             </template>
             <template #right>
                 <UButton icon="i-lucide-plus" to="/search">Add</UButton>
@@ -28,4 +35,5 @@
         </UMain>
     </UApp>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
