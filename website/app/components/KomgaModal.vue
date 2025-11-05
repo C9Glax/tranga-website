dcd78d0e036f4e50d1fd40961ae73babfd501d78
glax <johanna@bernloehr.eu>
2025-11-05 17:29:35 +0100

    Add Link to Komga-API-Keys section in Komga Modal

--- website/app/components/KomgaModal.vue
+++ website/app/components/KomgaModal.vue
@@ -2,16 +2,19 @@
     <UModal v-bind="$props" title="Connect Komga">
         <template #body>
             <UFormField label="URL">
-                <UInput v-model="requestData.url" placeholder="https://" class="w-full" :disabled="busy" />
+                <UInput v-model="requestData.url" placeholder="https://" class="w-full" />
             </UFormField>
             <UFormField label="ApiKey">
-                <UInput v-model="requestData.apiKey" class="w-full" :disabled="busy" />
+                <template #description>
+                    <ULink class="underline" :disabled="!apiLink" :to="apiLink" target="_blank" external no-prefetch>Get your API Key</ULink>
+                </template>
+                <UInput v-model="requestData.apiKey" class="w-full" />
             </UFormField>
             <UButton
                 icon="i-lucide-link"
                 :class="['mt-2 float-right', success == false ? 'animate-[shake_0.2s] bg-error' : '']"
-                :loading="busy"
-                :disabled="busy || !allowSend"
+                loading-auto
+                :disabled="!allowSend"
                 @click="connect"
                 >Connect</UButton
             >
@@ -28,11 +31,21 @@ const requestData = ref<CreateKomgaRecord>({ url: '', apiKey: '' });
 
 const allowSend = computed(() => requestData.value.url && requestData.value.apiKey);
 
-const busy = ref<boolean>(false);
+const apiLink = computed(() => {
+    console.log(requestData.value.url)
+    if (!requestData.value.url)
+        return undefined;
+    try {
+        const url = new URL(requestData.value.url);
+        return `${url.protocol}//${url.host}/account/api-keys`;
+    }catch{
+        return undefined;
+    }
+});
+
 const success = ref<boolean | undefined>(undefined);
 const emit = defineEmits<{ close: [boolean] }>();
 const connect = async () => {
-    busy.value = true;
     try {
         await $api('/v2/LibraryConnector/Komga', { method: 'PUT', body: requestData.value });
         await refreshNuxtData(FetchKeys.Libraries.All);
@@ -41,8 +54,6 @@ const connect = async () => {
     } catch {
         success.value = false;
         setTimeout(() => (success.value = undefined), 200);
-    } finally {
-        busy.value = false;
     }
 };
 </script>
