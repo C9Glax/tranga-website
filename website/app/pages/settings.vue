65698642abdcabc91cad4838915668c6fd78d4a7
glax <johanna@bernloehr.eu>
2025-11-07 00:43:20 +0100

    Add stats section to settings

--- website/app/pages/settings.vue
+++ website/app/pages/settings.vue
@@ -1,6 +1,6 @@
 <template>
     <TrangaPage>
-        <UPageSection title="Settings">
+        <UPageSection title="Settings" :ui="{ container: 'py-2 sm:py-2 lg:py-2 gap-2' }">
             <template #description>
                 <div v-if="settingsStatus === 'error'">
                     <p class="text-warning">Unable to connect to api.</p>
@@ -55,6 +55,16 @@
                 <div class="flex gap-2">
                     <UButton icon="i-lucide-database" loading-auto class="w-fit mb-2" @click="cleanUpDatabase">Clean database</UButton>
                     <UButton icon="i-lucide-captions-off" loading-auto class="w-fit mb-2" @click="cleanUpActions">Clean actions</UButton>
+                </div>
+            </UCard>
+            <UCard>
+                <template #header>
+                    <h1>Stats</h1>
+                </template>
+                <div class="flex flex-row flex-wrap gap-2">
+                    <UBadge v-for="(value, name) in stats" :key="name" variant="outline" color="neutral">
+                        {{ deCamel(name) }}: {{ value }}
+                    </UBadge>
                 </div>
             </UCard>
         </UPageSection>
@@ -120,6 +130,9 @@ const onKavitaClick = async () => {
 };
 
 const { status: settingsStatus } = useApi('/v2/Settings', { key: FetchKeys.Settings.All, server: false });
+
+const { data: stats } = useApi('/v2/Stats', { server: false });
+const deCamel = (camel : string) : string => camel.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
 
 useHead({ title: 'Settings' });
 </script>
